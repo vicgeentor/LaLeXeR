ee433a2ff206e58cd82a6996502d25c4891eb6ad
vicgeentor <github@vicgeentor.nl>
2026-03-14 11:29:43 +0100

    core: add and fix some printer combinators

--- src/Hattier/Printer/Combinators.hs
+++ src/Hattier/Printer/Combinators.hs
@@ -1,19 +1,38 @@
 module Hattier.Printer.Combinators where
 
-import Control.Monad.State
+import Control.Monad.RWS
 import Data.Text (Text)
+import qualified Data.Text as T
 import qualified Data.Text.Lazy.Builder as B
 import Hattier.Types
 
 append :: Text -> Hattier
+append "" = pure ()
 append txt = do
-  col <- gets currentColumn
-  modify'
-    (\s ->
-       s
-         { builder = builder s <> B.fromText txt
-         , currentColumn = currentColumn s + col
-         })
+  s <- get
+  let ind = currentIndent s
+      col = currentColumn s
+      spaces =
+        if col == 0
+          then T.replicate ind " "
+          else mempty
+      indentedTxt = spaces <> txt
+  modify' $ \s' ->
+    s'
+      { builder = builder s <> B.fromText indentedTxt
+      , currentColumn = currentColumn s + T.length indentedTxt
+      }
 
 newline :: Hattier
-newline = append "\n"
+newline = do
+  modify' $ \s -> s {builder = builder s <> "\n", currentColumn = 0}
+
+incrIndent :: Hattier
+incrIndent = do
+  width <- asks indentWidth
+  modify' $ \s -> s {currentIndent = currentIndent s + width}
+
+decrIndent :: Hattier
+decrIndent = do
+  width <- asks indentWidth
+  modify' $ \s -> s {currentIndent = max 0 (currentIndent s - width)}
